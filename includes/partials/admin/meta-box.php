e9b6e25a525ba6a935c63ef55db3406fd08460f9
Josh Justice <josh@need-bee.com>
2015-09-17 19:58:25 -0400

    adds handling for case where no tags set

--- includes/partials/admin/meta-box.php
+++ includes/partials/admin/meta-box.php
@@ -1,8 +1,12 @@
 <div id="primary-tag">
-    <select name="primary_tag">
-        <option value="">(none)</option>
-        <?php foreach( $tags as $tag ): ?>
-            <option value="<?php echo $tag->name ?>" <?php if( $tag->name == $primary_tag ) { echo 'selected'; } ?>><?php echo $tag->name ?></option>
-        <?php endforeach ?>
-    </select>
+    <?php if( empty($tags) ): ?>
+        To set a primary tag, first add tags and save the post.
+    <?php else: ?>
+        <select name="primary_tag">
+            <option value="">(none)</option>
+            <?php foreach( $tags as $tag ): ?>
+                <option value="<?php echo $tag->name ?>" <?php if( $tag->name == $primary_tag ) { echo 'selected'; } ?>><?php echo $tag->name ?></option>
+            <?php endforeach ?>
+        </select>
+    <?php endif ?>
 </div><!-- #primary-tag -->
