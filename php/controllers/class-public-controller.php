859c5af3a5a0a10fe630ac1aedd72c243d1c56cd
Josh Justice <josh@need-bee.com>
2015-09-17 20:25:06 -0400

    adds minified css file

--- php/controllers/class-public-controller.php
+++ php/controllers/class-public-controller.php
@@ -4,7 +4,7 @@ class PublicController extends BaseController
 {
 
     public function add_styles() {
-        wp_enqueue_style( 'primary-tag', plugins_url() . '/primary-tag/assets/css/primary-tag.css' );
+        wp_enqueue_style( 'primary-tag', plugins_url() . '/primary-tag/assets/css/primary-tag.min.css' );
     }
 
     public function render_primary_tag( $content ) {
