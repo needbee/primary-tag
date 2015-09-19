88dcdb953b60d346c4e2baea978863644ef6151d
Josh Justice <josh@need-bee.com>
2015-09-19 14:40:54 -0400

    fixes indentation to standard

--- includes/partials/admin/meta-box.php
+++ includes/partials/admin/meta-box.php
@@ -2,18 +2,18 @@
     <select name="primary_tag">
         <option value="">(none)</option>
         <?php foreach( $tags as $tag ): ?>
-            <option value="<?php echo esc_attr($tag->name) ?>" <?php if( $tag->name == $primary_tag ) { echo 'selected'; } ?>>
-                <?php
-                /*
-                 * Escape to protect against cross-site scripting.
-                 *
-                 * $primary_tag is also sanitized at time of input, but
-                 * sanitize_text_field() provides more protection since a tag should never
-                 * contain HTML.
-                 */
-                echo sanitize_text_field($tag->name)
-                ?>
-            </option>
+        <option value="<?php echo esc_attr($tag->name) ?>" <?php if( $tag->name == $primary_tag ) { echo 'selected'; } ?>>
+            <?php
+            /*
+             * Escape to protect against cross-site scripting.
+             *
+             * $primary_tag is also sanitized at time of input, but
+             * sanitize_text_field() provides more protection since a tag should never
+             * contain HTML.
+             */
+            echo sanitize_text_field($tag->name)
+            ?>
+        </option>
         <?php endforeach ?>
     </select>
 
