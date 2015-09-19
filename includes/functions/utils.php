5ab78887cc8963b152c40569f741a0e6cc6b1bd9
Josh Justice <josh@need-bee.com>
2015-09-19 19:41:16 -0400

    renames function since it no longer needs a prefix

--- includes/functions/utils.php
+++ includes/functions/utils.php
@@ -5,7 +5,7 @@ namespace NeedBee\Primary_Tag\Utils;
  * @uses error_log()
  * @uses WP_DEBUG
  */
-function pt_write_log( $log ) {
+function write_log( $log ) {
 	if ( true === WP_DEBUG ) {
 		if ( is_array( $log ) || is_object( $log ) ) {
 			error_log( print_r( $log, true ) );
