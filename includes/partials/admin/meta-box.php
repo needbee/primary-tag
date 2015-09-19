0b2c300ec40be4f01c4399bfe4966a8d21de46a1
Josh Justice <josh@need-bee.com>
2015-09-19 14:46:28 -0400

    updates spaces in partials to tabs

--- includes/partials/admin/meta-box.php
+++ includes/partials/admin/meta-box.php
@@ -1,21 +1,21 @@
 <div id="primary-tag">
-    <select name="primary_tag">
-        <option value="">(none)</option>
-        <?php foreach( $tags as $tag ): ?>
-        <option value="<?php echo esc_attr($tag->name) ?>" <?php if( $tag->name == $primary_tag ) { echo 'selected'; } ?>>
-            <?php
-            /*
-             * Escape to protect against cross-site scripting.
-             *
-             * $primary_tag is also sanitized at time of input, but
-             * sanitize_text_field() provides more protection since a tag should never
-             * contain HTML.
-             */
-            echo sanitize_text_field($tag->name)
-            ?>
-        </option>
-        <?php endforeach ?>
-    </select>
+	<select name="primary_tag">
+		<option value="">(none)</option>
+		<?php foreach( $tags as $tag ): ?>
+		<option value="<?php echo esc_attr($tag->name) ?>" <?php if( $tag->name == $primary_tag ) { echo 'selected'; } ?>>
+			<?php
+			/*
+			 * Escape to protect against cross-site scripting.
+			 *
+			 * $primary_tag is also sanitized at time of input, but
+			 * sanitize_text_field() provides more protection since a tag should never
+			 * contain HTML.
+			 */
+			echo sanitize_text_field($tag->name)
+			?>
+		</option>
+		<?php endforeach ?>
+	</select>
 
-    The primary tag will show at the top of the post page.
+	The primary tag will show at the top of the post page.
 </div><!-- #primary-tag -->
--- includes/partials/public/primary-tag.php
+++ includes/partials/public/primary-tag.php
@@ -1,17 +1,17 @@
 <?php if( $primary_tag ): ?>
 <div class="primary-tag-container">
-    <span class="screen-reader-text">Primary Tag</span>
-    <?php
-    /*
-     * Escape to protect against cross-site scripting.
-     *
-     * $primary_tag is also sanitized at time of input, but
-     * sanitize_text_field() provides more protection since a tag should never
-     * contain HTML.
-     */
-    ?>
-    <a href="<?php echo site_url('tags/'.$primary_tag.'/') ?>" class="primary-tag" rel="tag">
-    <?php echo sanitize_text_field($primary_tag) ?>
-    </a>
+	<span class="screen-reader-text">Primary Tag</span>
+	<?php
+	/*
+	 * Escape to protect against cross-site scripting.
+	 *
+	 * $primary_tag is also sanitized at time of input, but
+	 * sanitize_text_field() provides more protection since a tag should never
+	 * contain HTML.
+	 */
+	?>
+	<a href="<?php echo site_url('tags/'.$primary_tag.'/') ?>" class="primary-tag" rel="tag">
+	<?php echo sanitize_text_field($primary_tag) ?>
+	</a>
 </div>
 <?php endif ?>
