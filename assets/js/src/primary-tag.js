95a0ea83f78fd847d893e857de7e246ec5ac7c17
Josh Justice <josh@need-bee.com>
2015-09-19 17:03:18 -0400

    formats JS to standards

--- assets/js/src/primary-tag.js
+++ assets/js/src/primary-tag.js
@@ -11,30 +11,36 @@
 
 	function updatePrimaryTagDropdownOnDelay() {
 		setTimeout( function() {
-			var $select = jQuery('#primary-tag select');
+			var $select = jQuery( '#primary-tag select' );
 			var currentTag = $select.val();
 
 			// remove all tag options
-			$select.find('option:not(:first)').remove();
+			$select.find( 'option:not(:first)' ).remove();
 
 			// in case the primary tag was removed, select '(none)' by default
-			$select.val('');
+			$select.val( '' );
 
 			// add all current tags back in as options
 			var tags = getTags();
 			var option;
 			for( var i in tags ) {
-				option = jQuery('<option />').attr('value', tags[i]).text(tags[i]);
-				if( currentTag == tags[i] ) {
-					option.attr('selected',true);
+				option = jQuery( '<option />' )
+					.attr( 'value', tags[ i ] )
+					.text( tags[ i ] );
+				if( currentTag === tags[ i ] ) {
+					option.attr( 'selected', true );
 				}
-				option.appendTo($select);
+				option.appendTo( $select );
 			}
 		}, 100 );
 	}
 
 	function getTags() {
-		var tags = jQuery('div.tagchecklist span').map(function(i, elm){return jQuery(elm).text().substring(2);}).get();
+		var tags = jQuery( 'div.tagchecklist span' )
+			.map( function( i, elm ) {
+				return jQuery( elm ).text().substring( 2 );
+			} )
+			.get();
 		return tags;
 	}
 
@@ -42,11 +48,11 @@
 		// update the primary tag dropdown any time the tags change, either by:
 
 		// clicking the Add button
-		jQuery('input.tagadd').click(updatePrimaryTagDropdownOnDelay);
+		jQuery( 'input.tagadd' ).click( updatePrimaryTagDropdownOnDelay );
 
 		// hitting return while in the add tag box
-		jQuery('#new-tag-post_tag').keyup(function(evt) {
-			if( evt.keyCode == 13 ) {
+		jQuery( '#new-tag-post_tag' ).keyup( function( evt ) {
+			if ( 13 === evt.keyCode ) {
 				updatePrimaryTagDropdownOnDelay();
 			}
 		});
@@ -59,7 +65,7 @@
 		 * this does is refresh the tag list, it's not a big problem to do it for
 		 * _any_ click in the checklist area.
 		 */
-		jQuery('.tagchecklist').on('click', updatePrimaryTagDropdownOnDelay );
+		jQuery( '.tagchecklist' ).on( 'click', updatePrimaryTagDropdownOnDelay );
 	});
 
 } )( this );
