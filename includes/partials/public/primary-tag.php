617d51c91c23e61c854b9eb65a1c890c7b57d516
Josh Justice <josh@need-bee.com>
2015-09-19 05:12:44 -0400

    adds xss protection

--- includes/partials/public/primary-tag.php
+++ includes/partials/public/primary-tag.php
@@ -1,3 +1,14 @@
 <?php if( $primary_tag ): ?>
-    <div class="primary-tag"><?php echo $primary_tag ?></div>
+<div class="primary-tag">
+    <?php
+    /*
+     * Escape to protect against cross-site scripting.
+     *
+     * $primary_tag is also sanitized at time of input, but
+     * sanitize_text_field() provides more protection since a tag should never
+     * contain HTML.
+     */
+    ?>
+    <?php echo sanitize_text_field($primary_tag) ?>
+</div>
 <?php endif ?>
