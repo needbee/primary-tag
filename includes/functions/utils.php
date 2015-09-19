f18b31cb30ea5c6e810fb6c03aac0be63098e216
Josh Justice <josh@need-bee.com>
2015-09-19 16:33:01 -0400

    fixes code formatting issues

--- includes/functions/utils.php
+++ includes/functions/utils.php
@@ -1,7 +1,7 @@
 <?php
 
-if (!function_exists('pt_write_log')) {
-	function pt_write_log ( $log )  {
+if ( ! function_exists( 'pt_write_log' ) ) {
+	function pt_write_log( $log ) {
 		if ( true === WP_DEBUG ) {
 			if ( is_array( $log ) || is_object( $log ) ) {
 				error_log( print_r( $log, true ) );
--- php/class-primary-tag-repository.php
+++ php/class-primary-tag-repository.php
@@ -6,7 +6,7 @@ class Primary_Tag_Repository {
 
 	public function get_for_post( $post_id ) {
 		$tag_id = get_post_meta( $post_id, self::PRIMARY_TAG_KEY, true );
-		return get_term_by( 'id', $tag_id, 'post_tag' );
+		return get_term( $tag_id, 'post_tag' );
 	}
 
 	public function save_for_post( $post_id, $primary_tag_name ) {
@@ -14,7 +14,6 @@ class Primary_Tag_Repository {
 			delete_post_meta( $post_id, self::PRIMARY_TAG_KEY );
 		} else {
 			$tag = get_term_by( 'name', $primary_tag_name, 'post_tag' );
-			pt_write_log('got tag: '.print_r($tag,true));
 			update_post_meta( $post_id, self::PRIMARY_TAG_KEY, $tag->term_id );
 		}
 	}
