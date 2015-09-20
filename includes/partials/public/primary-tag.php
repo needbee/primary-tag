6ee92dce4ff59f31c44d0feccdf3e808309de0dd
Josh Justice <josh@need-bee.com>
2015-09-20 19:58:35 -0400

    removes comment that no longer applies

--- includes/partials/public/primary-tag.php
+++ includes/partials/public/primary-tag.php
@@ -13,10 +13,6 @@
 
 	/*
 	 * Escape to protect against cross-site scripting.
-	 *
-	 * $primary_tag is also sanitized at time of input, but
-	 * sanitize_text_field() provides more protection since a tag should never
-	 * contain HTML.
 	 */
 	?>
 	<a href="<?php echo esc_url( site_url( 'tag/'.$data['primary_tag']->slug.'/' ) ) ?>" class="primary-tag" rel="tag">
